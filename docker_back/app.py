a22e3cf3eb5bf779b237cbd547001e86c4ee7419
Vrouqen <luiselomi@outlook.com>
2025-04-16 18:50:17 -0500

    Se crea la base del backend

---
+++ docker_back/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+conexion1 = {
+    # Poner cadena de conexion
+
+}
+
+conexion2 = {
+    # Poner cadena de conexion
+
+}
+
+conexion3 = {
+    # Poner cadena de conexion
+
+}
+
+
+if __name__=="__main__":
+    app.run(debug=True, port=8081)
