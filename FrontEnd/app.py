6c27d454763c29fa8ae59bbbae2c957f168ac58a
Vrouqen <luiselomi@outlook.com>
2025-04-17 16:22:10 -0500

    Se implementa eliminación de usuarios utilizando el backend

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -8,8 +8,12 @@ def index():
     return render_template("index.html")
 
 @app.route("/insert_user")
-def insert_uset():
+def insert_user():
     return render_template("insert_user.html")
+
+@app.route("/delete_user")
+def delete_user():
+    return render_template("delete_user.html")
 
 if __name__=="__main__":
     app.run(debug=True, port=8080)
