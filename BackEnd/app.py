62f8310c047d409e7141864ddd9f273f370f4fe1
Vrouqen <luiselomi@outlook.com>
2025-04-17 15:17:54 -0500

    Se modifica el backend con CORS para que se pueda realizar las solicitudes de manera local

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for, request, jsonify
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
-
+CORS(app)
 
 conn1 = mysql.connector.connect(
     host='localhost',
